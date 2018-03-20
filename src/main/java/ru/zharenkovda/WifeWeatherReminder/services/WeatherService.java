00fd76186400cc7d814db8464920cf3b16bc006d
Danila_Zharenkov <zharenkov@lanit.ru>
2018-03-20 21:10:42 +0400

    switch weater service to darksky

--- src/main/java/ru/zharenkovda/WifeWeatherReminder/services/WeatherService.java
+++ src/main/java/ru/zharenkovda/WifeWeatherReminder/services/WeatherService.java
@@ -14,14 +14,15 @@ import java.net.URL;
 @Service
 public class WeatherService {
 
+    private String darkSkyUrl = "https://api.darksky.net/forecast/cf3f211102b3f5a195c897ff6b52980c/53.2,50.15?units=si&lang=ru";
     public String getWeatherString() throws IOException {
         String json = getWeatherJSON();
-        return parseJsonWeatherStringToText(json);
+        return parseJsonDarkSkyStringToText(json);
     }
 
     private String getWeatherJSON() throws IOException {
         StringBuilder jsonBuilder = new StringBuilder();
-        URL url = new URL("http://api.wunderground.com/api/8b0b570b1abd84a7/forecast/lang:RU/q/Russia/Samara.json");
+        URL url = new URL(darkSkyUrl);
         HttpURLConnection connection = (HttpURLConnection)url.openConnection();
         connection.setRequestMethod("GET");
         BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
@@ -32,34 +33,26 @@ public class WeatherService {
         return jsonBuilder.toString();
     }
 
-    private String parseJsonWeatherStringToText(String jsonString) throws IOException {
+    private String parseJsonDarkSkyStringToText(String jsonString) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-        JsonNode json = mapper.readTree(jsonString);
-        JsonNode simpleForecastToday = json.get("forecast").get("simpleforecast").get("forecastday").get(0);
-        JsonNode txtForecastToday = json.get("forecast").get("txt_forecast").get("forecastday").get(1);
+        JsonNode node = mapper.readTree(jsonString);
+        JsonNode currentData = node.get("currently");
+        String todaysSummaryData = node.get("hourly").get("summary").asText();
 
-        if (simpleForecastToday ==null){
-            return "";
-        }
-        String weekday = simpleForecastToday.get("date").get("weekday_short").asText();
-        String maxTemp = simpleForecastToday.get("high").get("celsius").asText();
-        String minTemp = simpleForecastToday.get("low").get("celsius").asText();
-        String windDir = simpleForecastToday.get("avewind").get("dir").asText();
-        Long windSpeed = Math.round(simpleForecastToday.get("avewind").get("kph").asDouble()*1000/3600);
-        String result = String.format("Сегодня (%s) максимальная температура %s ℃, минимальная температура %s℃. Ветер %s %d м/c. ",
-                weekday,maxTemp,minTemp,windDir,windSpeed);
-        //bug with fcttext_metric in weather.com service
-        if (txtForecastToday !=null) {
-            String nightInfoTitle = txtForecastToday.get("title").asText();
-            String nightInfo = txtForecastToday.get("fcttext_metric").asText();
-            if (StringUtils.isNotEmpty(nightInfoTitle) && StringUtils.isNotEmpty(nightInfo)) {
-                char[] nightInfoArr = nightInfo.toCharArray();
-                nightInfoArr[0] = Character.toLowerCase(nightInfoArr[0]);
-                nightInfo = new String(nightInfoArr).replace("переменчевый","переменчивый");
-                result = result + String.format(" %s %s", nightInfoTitle, nightInfo);
+        JsonNode todaysDailyData = node.get("daily").get("data").get(0);
 
-            }
-        }
+        Long currentTemp = Math.round(currentData.get("temperature").asDouble());
+        String currentState = currentData.get("summary").asText().toLowerCase();
+        Long currentApparentTemp = Math.round(currentData.get("apparentTemperature").asDouble());
+        Long maxTemp = Math.round(todaysDailyData.get("temperatureHigh").asDouble());
+        Long minTemp = Math.round(todaysDailyData.get("temperatureLow").asDouble());
+
+        Long windGust = Math.round(todaysDailyData.get("windGust").asDouble());
+        Long windSpeed = Math.round(todaysDailyData.get("windSpeed").asDouble());
+        String result = String.format("Сегодня %s, температура воздуха %d℃, ощущается как %d℃. " +
+                "Максимальная температура %d℃, минимальная температура %d℃. Скорость ветра %dм/с с порывами до %dм/с. %s",
+                currentState,currentTemp,currentApparentTemp,maxTemp,minTemp,windSpeed,windGust,todaysSummaryData);
+
         return result;
     }
 }
